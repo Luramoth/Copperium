5d81695879b7039c3f28b1c69de4b7e63deef55e
LuraMoth <85266594+Luramoth@users.noreply.github.com>
2023-08-09 01:29:07 -0700

    todo comments

--- src/main/java/com/luramoth/copperium/block/custom/infused_copper_block.java
+++ src/main/java/com/luramoth/copperium/block/custom/infused_copper_block.java
@@ -9,6 +9,13 @@ import net.minecraft.state.properties.BlockStateProperties;
 
 import javax.annotation.Nullable;
 
+// conductable pathing: prioritise going in the same direction
+// if cant continue in the same direction, pick a random available direction
+// if infused block is found, dump all power into it
+//
+// infused block: fill up power with any recieved by conductables
+// if there is excess power, dump it into adjacent infused block, if there isent one, discard excess power
+
 public class infused_copper_block extends Block {
     public static final IntegerProperty POWER = IntegerProperty.create("power", 0, 14);
 
